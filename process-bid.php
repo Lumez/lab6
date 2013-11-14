176225ab1a4c345d05fb0bcae2f6ba9d1368e821
Ben <lumezious@googlemail.com>
2013-11-14 15:50:38 +0000

    Added SQL INSERT to add bid to bid table
    Added SQL INSERT query to add bid from post data to the bid table.

--- process-bid.php
+++ process-bid.php
@@ -2,7 +2,10 @@
 <!-- PART 1 PROCESS BID PLACEHOLDER -->
 <!-- ============================== -->
 <?php 
-	
+	require('includes/db.php');
+
 	print_r($_POST);
+	
+	$bid = $db->query("INSERT INTO bid (car_id, name, value, datetime) VALUES (' {$_POST['car_id']} ', '{$_POST['name']}', '{$_POST['value']}', NOW())");
 
 ?>
